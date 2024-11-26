a1253ee7903b2872b761f6c1b95f8202c926b5e9
saul <carpiojassosaul4@gmail.com>
2024-11-25 22:23:08 -0600

    tarea

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,22 +7,30 @@ import { Geolocation } from '@capacitor/geolocation';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  locations: { latitud: number; longitud: number; date: Date}[]=[];
+  locations: { latitud: number; longitud: number; date: string }[] = [];
 
   constructor() {}
- 
-   async getLocation() {
-    try{
+
+  async getLocation() {
+    try {
       const position = await Geolocation.getCurrentPosition();
+      const now = new Date();
       const location = {
         latitud: position.coords.latitude,
         longitud: position.coords.longitude,
-        date: new Date()
+        date: `${String(now.getDate()).padStart(2, '0')}/${String(
+          now.getMonth() + 1
+        ).padStart(2, '0')}/${now.getFullYear()}`,
       };
-      this.locations.push(location)
-      console.log(this.locations)
-    }catch(e){
-      console.log('Error getting the location',e);
+      this.locations.push(location);
+      console.log(this.locations);
+    } catch (e) {
+      console.log('Error getting the location', e);
     }
   }
+
+  resetLocations() {
+    this.locations = [];
+  }
 }
+
